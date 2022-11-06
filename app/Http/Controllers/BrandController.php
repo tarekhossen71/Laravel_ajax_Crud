4faffb4666e5b09a82bb8714b61041e5975354df
tarekhossen71 <tarekhossen.offi@gmail.com>
2022-11-06 13:03:24 +0600

    Laravel Crud brand and category add comments

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,15 +7,31 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
+    /**
+     * Brand Resource List
+     * 
+     * @return Illuminate\Http\Response
+     */
     public function index(){
         $brands = Brand::orderBy('id', 'desc')->get();
         return view('dashboard.brands.index',['brands'=>$brands]);
     }
 
+    /**
+     * Brand Resource Create
+     * 
+     * @return Illuminate\Http\Response
+     */
     public function create(){
         return view('dashboard.brands.create');
     }
 
+    /**
+     * Brand Resource Store
+     * 
+     * @param Illuminate\Http\Request $request
+     * @return Illuminate\Http\Response
+     */
     public function store(Request $request){
         $request->validate([
             'brand_name' => ['required', 'string', 'min:2', 'max:100'],
@@ -30,10 +46,23 @@ class BrandController extends Controller
         return back()->with('success', 'Brand has been created!');
     }
 
+    /**
+     * Brand Resource Edit
+     * 
+     * @param App\Model\Brand $brand
+     * @return Illuminate\Http\Response
+     */
     public function edit(Brand $brand){
         return view('dashboard.brands.edit', ['brand'=>$brand]);
     }
 
+    /**
+     * Brand Resource Update
+     * 
+     * @param App\Model\Brand $brand
+     * @param Illuminate\Http\Request $request
+     * @return Illuminate\Http\Response
+     */
     public function update(Request $request, Brand $brand){
         $request->validate([
             'brand_name' => ['required', 'string', 'min:2', 'max:100'],
@@ -48,6 +77,12 @@ class BrandController extends Controller
         return back()->with('success', 'Brand has been updated!');
     }
 
+    /**
+     * Brand Resource Destroy
+     * 
+     * @param App\Model\Brand $brand
+     * @return Illuminate\Http\Response
+     */
     public function destroy(Brand $brand){
         $brand->delete();
         return back()->with('success', 'Brand has been deleted');
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,15 +7,31 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * Category Resource List
+     * 
+     * @return Illuminate\Http\Response
+     */
     public function index(){
         $categories = Category::orderBy('id', 'desc')->get();
         return view('dashboard.categories.index',['categories'=>$categories]);
     }
 
+    /**
+     * Category Resource Create
+     * 
+     * @return Illuminate\Http\Response
+     */
     public function create(){
         return view('dashboard.categories.create');
     }
 
+    /**
+     * Category Resource Store
+     * 
+     * @param Illuminate\Http\Request $request
+     * @return Illuminate\Http\Response
+     */
     public function store(Request $request){
         $request->validate([
             'category_name' => ['required', 'string', 'min:2', 'max:100'],
@@ -30,10 +46,23 @@ class CategoryController extends Controller
         return back()->with('success', 'Category has been created!');
     }
 
+    /**
+     * Category Resource Edit
+     * 
+     * @param App\Model\Category $category
+     * @return Illuminate\Http\Response
+     */
     public function edit(Category $category){
         return view('dashboard.categories.edit', ['category'=>$category]);
     }
 
+    /**
+     * Category Resource Update
+     * 
+     * @param App\Model\Category $category
+     * @param Illuminate\Http\Request $request
+     * @return Illuminate\Http\Response
+     */
     public function update(Request $request, Category $category){
         $request->validate([
             'category_name' => ['required', 'string', 'min:2', 'max:100'],
@@ -48,6 +77,12 @@ class CategoryController extends Controller
         return back()->with('success', 'Category has been updated!');
     }
 
+    /**
+     * Category Resource Destroy
+     * 
+     * @param App\Model\Category $category
+     * @return Illuminate\Http\Response
+     */
     public function destroy(Category $category){
         $category->delete();
         return back()->with('success', 'Category has been deleted');
